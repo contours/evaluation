54b3d4184992cf8e6ed0287b0b8af0a9d576f96e
Ryan Shaw <ryanshaw@unc.edu>
2013-02-15 11:15:37 -0500

    Added scripts for generating speaker and speechblock segmentations.

---
+++ speakers.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+
+import redis
+import json
+from argparse import ArgumentParser
+from utils import *
+
+def speaker_masses(r, interview):
+  speakers = [ r.hget(s, 'speaker') for s in r.lrange(
+      'interviews:{}:sentences'.format(interview), 0, -1) ]
+  print 
+  indexes = [ i for i in 
+              map(lambda pair: None if pair[1][0] == pair[1][1] else pair[0],
+                  enumerate(pairwise(speakers), start=1)) if i is not None ]
+  [masses] = indexes_to_masses([indexes], len(speakers))
+  return masses
+
+def parse_args():
+  p = ArgumentParser(description=main.__doc__)
+  p.add_argument('interviews', help='interviews to get speaker masses of',
+                 action='append')
+  return p.parse_args()
+
+def main():
+  args = parse_args()
+  r = redis.StrictRedis()
+  o = { 'id':'speakers', 
+        'segmentation_type':'linear',
+        'items':
+          { 'interviews:{}'.format(interview): 
+            { 'speakers':speaker_masses(r, interview) }
+            for interview in args.interviews }}
+  print json.dumps(o, sort_keys=True)
+
+if __name__ == "__main__":
+  main()
---
+++ speechblocks.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+import redis
+import json
+from argparse import ArgumentParser
+
+def speechblock_mass(r, speechblock):
+  mass = r.llen('{}:sentences'.format(speechblock))
+  assert mass > 0, 'speechblock {} has zero mass'.format(speechblock)
+  return mass
+
+def speechblock_masses(r, interview):
+  speechblocks = 'interviews:{}:speechblocks'.format(interview)
+  return [ speechblock_mass(r,k) for k in r.lrange(speechblocks, 0, -1) ]
+
+def parse_args():
+  p = ArgumentParser(description=main.__doc__)
+  p.add_argument('interviews', help='interviews to get speechblock masses of',
+                 action='append')
+  return p.parse_args()
+
+def main():
+  args = parse_args()
+  r = redis.StrictRedis()
+  o = { 'id':'speechblocks', 
+        'segmentation_type':'linear',
+        'items':
+          { 'interviews:{}'.format(interview): 
+            { 'speechblocks':speechblock_masses(r, interview) }
+            for interview in args.interviews }}
+  print json.dumps(o, sort_keys=True)
+
+if __name__ == "__main__":
+  main()
